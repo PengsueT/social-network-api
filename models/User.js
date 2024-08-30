bd32d1c7efcabc7a0ce04812297f17f8fa27ce93
Charles Thao <thaocharles23@gmail.com>
2024-08-30 18:36:22 -0500

    Created User model schema and Set up MongoDB connection in config

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/, 'Must match a valid email address!'] // used SaturnCloud source for this regular expression for email validation
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount'.length(function () {
+    return this.friends.length;
+}));
+
+const User = model('User', userSchema);
+
+module.exports = User;
