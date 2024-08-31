08bd659676f0e892ca12f5613e700eee2f13c142
Charles Thao <thaocharles23@gmail.com>
2024-08-30 19:37:17 -0500

    Created thought model schema and Reaction schema type

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const { Schema, Type } = require('mongoose');
+const dayjs = require('dayjs');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Types.ObjectId,
+        default: () => new Types.ObjectId(), // Default value set to a new ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dayjs(timestamp).format('MMM D, YYYY'), // Getter method to format the timestamp
+    },
+    },
+    {
+    toJSON: {
+        getters: true,
+    },
+    id: false, // Prevents the creation of a virtual id field
+});
+
+module.exports = reactionSchema;
